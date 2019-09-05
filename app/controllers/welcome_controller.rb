733f81caf7bbf0532f2641be27c07726b23fb566
bdou <bridget.dou@gmail.com>
2019-09-05 19:25:19 -0400

    basic error handling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,10 @@
-require 'twitter'
-
 class WelcomeController < ApplicationController
-
-	$client = Twitter::REST::Client.new do |config|
-	  config.consumer_key        = "LX92DpJ2UmYXRlA3GsPtrPEWp"
-	  config.consumer_secret     = "lRrpDXI6MBKs3STywjxhaC2c2ULYGGSrUNdmTDBpD9T79IGJj1"
-	  config.access_token        = "1169710228843220992-Fpn2wt1GE7SEjhqGcFqVx2VhhdItxc"
-	  config.access_token_secret = "Uh3oJ8ZHdDxmFQsIB36taKo7ZqaCPNanzho9VCy0V1PnA"
-	end
-
   def index
-	  @tweets = $client.user_timeline(params[:name], count: 25)
+  	begin
+		  @tweets = $client.user_timeline(params[:name], count: 25)
+		rescue
+			@tweets;
+			@error = "User not found"
+		end
   end
-
 end
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,8 @@
+require 'twitter'
+
+$client = Twitter::REST::Client.new do |config|
+	  config.consumer_key        = "LX92DpJ2UmYXRlA3GsPtrPEWp"
+	  config.consumer_secret     = "lRrpDXI6MBKs3STywjxhaC2c2ULYGGSrUNdmTDBpD9T79IGJj1"
+	  config.access_token        = "1169710228843220992-Fpn2wt1GE7SEjhqGcFqVx2VhhdItxc"
+	  config.access_token_secret = "Uh3oJ8ZHdDxmFQsIB36taKo7ZqaCPNanzho9VCy0V1PnA"
+end
